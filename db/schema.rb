5a0c0a816b37c9e096bb6eae6b3e26036a1bbb21
Alexandre Slim <alexandreslim@yahoo.fr>
2021-02-22 16:58:48 +0100

    create factory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_140710) do
+ActiveRecord::Schema.define(version: 2021_02_22_154330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "factories", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "speciality"
+    t.integer "production_capacity"
+    t.integer "moq"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
