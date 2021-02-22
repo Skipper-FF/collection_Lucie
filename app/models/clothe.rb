fc290fd740db68e618f257df9a7e5f6b6fc2fd44
Lucie Camail <luciecamail@gmail.com>
2021-02-22 17:48:10 +0100

    create technical detail model

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -2,4 +2,6 @@ class Clothe < ApplicationRecord
   belongs_to :factory
   belongs_to :season
   belongs_to :pattern
+
+  has_many :technical_details
 end
---
+++ app/models/technical_detail.rb
@@ -0,0 +1,4 @@
+class TechnicalDetail < ApplicationRecord
+  belongs_to :clothe
+  belongs_to :component
+end
