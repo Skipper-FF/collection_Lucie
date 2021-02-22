7c9f5484d5a690589408f0a30467e00e767dddbe
Alexandre Slim <alexandreslim@yahoo.fr>
2021-02-22 17:24:48 +0100

    create seasons model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_154330) do
-
+ActiveRecord::Schema.define(version: 2021_02_22_162254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "factories", force: :cascade do |t|
     t.string "name"
@@ -24,12 +22,33 @@ ActiveRecord::Schema.define(version: 2021_02_22_154330) do
     t.integer "production_capacity"
     t.integer "moq"
     t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "families", force: :cascade do |t|
     t.string "name"
-
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patterns", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_patterns_on_family_id"
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.string "name"
+    t.date "begining_date"
+    t.date "ending_date"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_seasons_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +63,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_154330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "patterns", "families"
+  add_foreign_key "seasons", "users", column: "users_id"
 end
