7c9d0e13cce71bcb76ca832b6afb8181dc085685
Alexandre Slim <alexandreslim@yahoo.fr>
2021-02-23 18:17:04 +0100

    season controller et ses index & show ET clothes controller et ses index show new

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,2 +1,29 @@
 class ClothesController < ApplicationController
+  def index
+    @clothes = Clothe.all
+  end
+
+  def show
+    @clothe = Clothe.find(params[:id])
+    @season = Season.find(params[:season_id])
+  end
+
+  def new
+    @clothe = Clothe.new
+  end
+
+  def create
+    @clothe = Clothe.new(cloth_params)
+    if @clothe.save
+      redirect_to season_clothe_path(@clothe.season, @clothe)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def cloth_params
+    params.require(:clothe).permit(:factory_id, :season_id, :pattern_id, :name, :reference, :quantity, :confection_cost, :total_cost, :selling_price)
+  end
 end
