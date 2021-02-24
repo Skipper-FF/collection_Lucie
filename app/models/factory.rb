40301e79e928cd7b4381103d18adc2801fbcde31
Alexandre Slim <alexandreslim@yahoo.fr>
2021-02-24 15:43:29 +0100

    update controller factories et ses edit index new show ET model factory ET controller families et ses edit index ET routes.rb

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,3 +1,6 @@
 class Factory < ApplicationRecord
     has_many :clothes
+
+    validates :name, uniqueness: true, presence: true
+    validates :production_capacity, numericality: {greater_than: 0}
 end
