4e5fedad0433744ec350b43cbc86b5b8f40fb236
Lucie Camail <luciecamail@gmail.com>
2021-03-02 03:41:03 +0100

    js for factories

--- app/javascript/components/showPatterns.js
+++ app/javascript/components/showPatterns.js
@@ -1,25 +1,26 @@
 const showPatterns = () => {
 
+    const fetchElements = (url, containerId) => {
+        fetch(url, { headers: { accept: "application/json" } })
+            .then(response => response.json())
+            .then((data) => {
+              const container = document.querySelector(containerId);
+              container.innerHTML = data.html;
+            })
+    } 
+
     const familyFields = document.querySelectorAll('input[name="clothe[family]"]');
     familyFields.forEach( (familyField) => {
       familyField.addEventListener( 'change', (event) => {
         if (event.currentTarget.checked) {
             const familyValue = event.currentTarget.value;
-            const url = `/patterns?id=${familyValue}`;
-            console.log(url);
-            fetch(url, { headers: { accept: "application/json" } })
-            .then(response => response.json())
-            .then((data) => {
-              const patternsContainer = document.querySelector('#pattern-fieldset');
-              patternsContainer.innerHTML = data.html;
-            });
+            const patternsUrl = `/patterns?id=${familyValue}`;
+            const factoriesUrl = `/factories?id=${familyValue}`;
+            fetchElements(patternsUrl, '#pattern-fieldset');
+            fetchElements(factoriesUrl, '#factory-fieldset');
         };
-
-
       });
     });
 }
 
 export { showPatterns };
-
-// #new_clothe > fieldset > div:nth-child(3)
