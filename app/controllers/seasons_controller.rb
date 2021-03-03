1f0245e4b7b01f82faa803bbdecd7d842f425389
Alexandre Baldelli <alexandre.baldelli@yahoo.fr>
2021-03-03 12:48:46 +0100

    Homage-change

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -5,12 +5,14 @@ class SeasonsController < ApplicationController
 
 	def show
 	  @season = Season.find(params[:id])
+    @seasons = Season.all
 	 if params[:query].present?
       @clothes = Clothe.search_by_name_and_reference(params[:query])
     else
       @clothes = @season.clothes
     end
 	end
+
 
 	def new
 	  @season = Season.new
